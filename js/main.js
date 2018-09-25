314f1ed61abc0d795203b103a4ad4458d4efc724
egor1k <egorka.and@gmail.com>
2018-09-25 20:47:13 +0300

    wow + @maincolor

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(window).on('load', function() {
-    $('h1').addClass('animated fadeInLeft');
-    $('h2').addClass('animated fadeIn delay-1s');
+    $("#preloader").delay(800).fadeOut('slow');
+    $('header').addClass('animated fadeInDown delay-1s');
+    $('h1').addClass('animated fadeInLeft delay-1s');
+    $('h2').addClass('animated fadeIn delay-2s');
+    $('#s2 .console img').addClass('wow fadeInLeft');
+    $('#s2 .console-body').addClass('wow fadeInRight');
+    new WOW().init();
+});
+
+$(document).ready(function () {
+    $(document).scroll(function () { 
+        if ($(this).scrollTop() > 0) {
+            $('header').addClass('active');
+        } else {
+            $('header').removeClass('active');
+        }
+    });
 });
