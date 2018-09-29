debd0b0d3951569187fa976c8866898a00d32715
egor1k <egorka.and@gmail.com>
2018-09-29 21:02:43 +0300

    titles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $(window).on('load', function() {
     $("#preloader").delay(800).fadeOut('slow');
+    $('#s1').addClass('animated fadeIn delay-1s slow');
     $('header').addClass('animated fadeInDown delay-1s');
-    $('h1').addClass('animated fadeInLeft delay-1s');
-    $('h2').addClass('animated fadeIn delay-2s');
-    $('#s2 .console img').addClass('wow fadeInLeft');
-    $('#s2 .console-body').addClass('wow fadeInRight');
+    $('#s1 h1').addClass('animated fadeInLeft delay-1s');
+    $('#s1 h2').addClass('animated fadeIn delay-2s');
+
+    $('.section-title').addClass('wow fadeInUp slow').attr('data-wow-offset', '150');
+    $('#s2 .console img').addClass('wow fadeInLeft').attr('data-wow-offset', '250');
+    $('#s2 .console-body').addClass('wow fadeInRight').attr('data-wow-offset', '250');
     new WOW().init();
 });
 
