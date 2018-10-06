2b664df5f6931dc96c566c714ce7b3882a1d89e4
egor1k <egorka.and@gmail.com>
2018-10-06 13:02:41 +0300

    psxfood

---
+++ PsXFood/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.hamburger, .panel-close').click(function (e) { 
+        e.preventDefault();
+        $('.panel').toggleClass('active');
+    });
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 1199,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+});
