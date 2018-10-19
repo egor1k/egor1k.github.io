0950c24ed0c01d6d27668afcec53d4be35fe9bbd
egor1k <egorka.and@gmail.com>
2018-10-19 15:11:36 +0300

    time

--- ArmoredTable/js/main.js
+++ ArmoredTable/js/main.js
@@ -8,37 +8,88 @@ var calendar = function () {
 }
 
 var slidePage = function () {
-    $(document).ready(function () {
 
-        var bsbody = $('body'),
-            buttonNext = $('.bs-next'),
-            buttonPrev = $('.bs-back'),
-            bsquantity = 3;
+    var bsbody = $('.bs-content'), // отслеживаемый контейнер
+        buttonNext = $('.bs-next'), // кнопка вперед
+        buttonPrev = $('.bs-back'), // кнопка назад
+        bsquantity = 3; // всего слайдов
 
-        buttonNext.on('click', function (e) {
-            e.preventDefault();
-            var slidenumber = bsbody.attr('data-bs');
-            if (slidenumber >= bsquantity) {
-                return;
+    bsbody.addClass('bs-2').attr('data-bs', '2');
+
+    buttonNext.on('click', function (e) {
+        e.preventDefault();
+        var slidenumber = bsbody.attr('data-bs');
+        if (slidenumber >= bsquantity) {
+            return;
+        }
+        var oldBsClass = "bs-" + slidenumber++,
+            newBsClass = "bs-" + slidenumber;
+        bsbody.attr('data-bs', slidenumber).removeClass(oldBsClass).addClass(newBsClass);
+    });
+
+    buttonPrev.on('click', function (e) {
+        e.preventDefault();
+        var slidenumber = bsbody.attr('data-bs');
+        if (slidenumber <= 1) {
+            return;
+        }
+        var oldBsClass = "bs-" + slidenumber--,
+            newBsClass = "bs-" + slidenumber;
+        bsbody.attr('data-bs', slidenumber).removeClass(oldBsClass).addClass(newBsClass);
+    });
+
+}
+
+var plusMinus = function () {
+
+    $('.bs-input .bs-plus').on('click', function (e) {
+        e.preventDefault();
+        var input = $(this).next('input');
+        var inputValue = input.val();
+
+        if (input.hasClass('bsMinute')) {
+            if (inputValue == 00) {
+                input.val('30');
+            } else {
+                input.val('00');
             }
-            var oldBsClass = "bs-" + slidenumber++,
-                newBsClass = "bs-" + slidenumber;
-            bsbody.attr('data-bs', slidenumber).removeClass(oldBsClass).addClass(newBsClass);
-        });
-
-        buttonPrev.on('click', function (e) {
-            e.preventDefault();
-            var slidenumber = bsbody.attr('data-bs');
-            if (slidenumber <= 1) {
-                return;
+        } else if (input.hasClass('bsHour')) {
+            if (inputValue < 24) {
+                input.val(++inputValue);
+            } else {
+                input.val(1);
             }
-            var oldBsClass = "bs-" + slidenumber--,
-                newBsClass = "bs-" + slidenumber;
-            bsbody.attr('data-bs', slidenumber).removeClass(oldBsClass).addClass(newBsClass);
-        });
+        } else {
+            input.val(++inputValue);
+        }
 
+    });
+
+    $('.bs-input .bs-minus').on('click', function (e) {
+        e.preventDefault();
+        var input = $(this).prev('input');
+        var inputValue = input.val();
+
+        if (input.hasClass('bsMinute')) {
+            if (inputValue == 00) {
+                input.val('30');
+            } else {
+                input.val('00');
+            }
+        } else if (input.hasClass('bsHour')) {
+            if (inputValue == 1) {
+                input.val(24);
+            } else {
+                input.val(--inputValue);
+            }
+        } else {
+            if (inputValue > 1) {
+                input.val(--inputValue);
+            }
+        }
     });
 }
 
 calendar();
 slidePage();
+plusMinus();
