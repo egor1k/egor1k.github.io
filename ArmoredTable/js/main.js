ade3d622f1d7455e1c7efd3efcc8cb9ede1eb399
egor1k <egorka.and@gmail.com>
2018-10-19 09:02:47 +0300

    fr

--- ArmoredTable/js/main.js
+++ ArmoredTable/js/main.js
@@ -4,3 +4,17 @@ datepicker.container = document.querySelector('#bs-calendar');
 datepicker.inline = true;
 datepicker.lang = 'ru';
 datepicker.show();
+
+
+
+$('.bs-content.bs-1 .bs-next').on('click', function(e) {
+    e.preventDefault();
+    $('.bs-content').removeClass('bs-1');
+    $('.bs-content').addClass('bs-2');
+});
+
+$('.bs-content.bs-2 .bs-prev').on('click', function(e) {
+    e.preventDefault();
+    $('.bs-content').removeClass('bs-2');
+    $('.bs-content').addClass('bs-1');
+});
