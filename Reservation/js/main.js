717ee34c58e8a7306d70824aeddfa83831102fb8
egor1k <egorka.and@gmail.com>
2018-10-23 10:14:58 +0300

    123

--- Reservation/js/main.js
+++ Reservation/js/main.js
@@ -14,36 +14,77 @@ var trSteps = function () {
         stepsBody = maincontainer.find('.tr-main'), // указать контейнер в котором находятся слайды
         nextbtn = $('.tr-footer__btn_next'), // кнопка вперед
         prevbtn = $('.tr-footer__btn_prev'), // кнопка назад
-        stepmax = stepsBody.children().length;
+        stepmax = stepsBody.children().length; // сколько всего шагов
 
-    maincontainer.attr('data-step', '4');
+    maincontainer.attr('data-step', '1');
     var stepNumber = maincontainer.attr('data-step');
 
-    var switchStep = function () {
+    var showHide = function () { // скрыть все шаги, кроме активного
         stepsBody.children().hide().eq(--stepNumber).show();
     }
 
-    switchStep();
+    showHide();
+
+    var switchStepPrev = function () { // вернуться на предыдущий шаг
+        maincontainer.attr('data-step', --stepNumber);
+        showHide();
+    }
+    var switchStepNext = function () { // перейти к следующему шагу
+        maincontainer.attr('data-step', ++stepNumber);
+        showHide();
+    }
+
+    var mainformsend = function () {
+        var send = true,
+            mainform = $('#tr-mainform');
+
+        mainform.find('input').each(function () { // проверка input на пустоту
+            if (!$(this).val() || $(this).val() == '') {
+                $(this).css('border', '1px solid red');
+                send = false;
+            } else {
+                $(this).css('border', 'none');
+            }
+        });
+
+        var checkbox = mainform.find('input:checkbox'),
+            checkboxspan = checkbox.next('span');
+
+        if (checkbox.prop('checked') == false) { // проверка чекбокса
+            checkboxspan.css('color', 'red');
+            send = false;
+        } else {
+            checkboxspan.css('color', 'white');
+        }
+
+        if (!send) return false;
+
+        switchStepNext();
+        $('.tr-header__h1').text('стол забронирован');
+        prevbtn.hide();
+        nextbtn.text('Вернуться на сайт');
+
+    };
 
     nextbtn.on('click', function (e) {
         e.preventDefault();
         stepNumber = maincontainer.attr('data-step');
-        if (stepNumber >= stepmax) {
-            return;
+        if (stepNumber >= stepmax) { // если шаг последний
+            return false;
+        } else if (stepNumber == 4) {
+            mainformsend();
         } else {
-            maincontainer.attr('data-step', ++stepNumber);
-            switchStep();
+            switchStepNext();
         }
     });
 
     prevbtn.on('click', function (e) {
         e.preventDefault();
         stepNumber = maincontainer.attr('data-step');
-        if (stepNumber <= 1) {
-            return;
+        if (stepNumber <= 1) { // если шаг первый
+            return false;
         } else {
-            maincontainer.attr('data-step', --stepNumber);
-            switchStep();
+            switchStepPrev();
         }
     });
 
@@ -78,9 +119,10 @@ var trCalendar = function () {
     datepicker.maxSelections = 1;
     datepicker.lang = 'ru';
     datepicker.container = document.querySelector('.tr-calendar');
-    datepicker.show();
+    datepicker.today = false;
     datepicker.daynames = false;
-    datepicker.today = true;
+    datepicker.show();
+    datepicker.selectDate(new Date());
 };
 
 var trInputToggle = function () {
