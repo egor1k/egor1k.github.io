d59146aa3e60838e6cdcd56156e5c49da0d9bcc4
egor1k <george.mainbox@gmail.com>
2019-01-29 13:06:34 +0300

    overflow

--- mfc2/js/script.js
+++ mfc2/js/script.js
@@ -1,103 +1,132 @@
 "use strict";
 
-(function ($) {
+(function ssInit() {
+    // clPreload();
+    clHeader();
+    clAnchors();
+    clOverlay();
+})();
 
-    function clHeader() {
-        var body = $('body'),
-            className = "header-is-smaller";
+function clHeader() {
+    var body = $('body'),
+        className = "header-is-smaller";
 
-        $(document).scroll(function () {
-            if ($(this).scrollTop() > 0) {
-                body.addClass(className);
-            } else {
-                body.removeClass(className);
-            }
-        });
-    }
-
-    function clAnchors() {
-        var header = $('.header'),
-            window = $('.push-window');
-
-        $('.js-anchor').on('click', function (e) {
-            e.preventDefault();
-            var headerHeight = header.innerHeight();
-
-            $('html, body').stop().animate({
-                scrollTop: $(this.hash).offset().top - 64
-            }, 800);
-
-            window.removeClass('is-open');
-            removeOverlay();
-        });
-    }
-
-    function clPreload() {
-        $("html").addClass('cl-preload');
-
-        $(window).on('load', function () {
-            // $('html, body').animate({
-            //     scrollTop: 0
-            // }, 'normal');
-
-            $("#loader").fadeOut("slow", function () {
-                $("#preloader").delay(300).fadeOut("slow");
-            });
-
-            $("html").removeClass('cl-preload');
-            $("html").addClass('cl-loaded');
-
-        });
-
-    }
-
-    function clOverlay() {
-        var windows = $('.push-window');
-
-        $('body').on('click', '#overlay', function (e) {
-            e.preventDefault();
-            windows.removeClass('is-open');
-            removeOverlay();
-        });
-    }
-
-    (function ssInit() {
-        // clPreload();
-        clHeader();
-        clAnchors();
-        clOverlay();
-    })();
-
-})(jQuery);
-
-function toggleWindow(num, that) {
-    if (num == 1) {
-        var window = $('#w1');
-        window.toggleClass('is-open');
-    } else if (num == 2) {
-        var window = $('#w2'),
-            itemName = that.closest('.shop-card').find('.shop-card__name').text(),
-            title = $('#w2 .push-window__title');
-        if (itemName) {
-            title.text("Заказать " + itemName);
+    $(document).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            body.addClass(className);
         } else {
-            title.text('');
+            body.removeClass(className);
         }
-        window.toggleClass('is-open');
+    });
+}
+
+function clAnchors() {
+    var header = $('.header'),
+        window = $('.push-window');
+
+    $('.js-anchor').on('click', function (e) {
+        e.preventDefault();
+        var headerHeight = header.innerHeight();
+
+        $('html, body').stop().animate({
+            scrollTop: $(this.hash).offset().top - 64
+        }, 800);
+
+        window.removeClass('is-open');
+        removeOverlay();
+    });
+}
+
+function clPreload() {
+    $("html").addClass('cl-preload');
+
+    $(window).on('load', function () {
+        // $('html, body').animate({
+        //     scrollTop: 0
+        // }, 'normal');
+
+        $("#loader").fadeOut("slow", function () {
+            $("#preloader").delay(300).fadeOut("slow");
+        });
+
+        $("html").removeClass('cl-preload');
+        $("html").addClass('cl-loaded');
+
+    });
+
+}
+
+function clOverlay() {
+    var windows = $('.push-window');
+
+    $('body').on('click', '.overlay', function (e) {
+        e.preventDefault();
+        windows.removeClass('is-open');
+        removeOverlay();
+    });
+}
+
+function toggleSubForm(id) {
+    $('.push-window').removeClass('is-open');
+    removeOverlay();
+    var win = $('#w3');
+
+    if (id == 0) {
+        win.removeClass('is-open');
+        toggleOverlay(win);
+        return;
     }
 
-    if (window.hasClass('is-open')) {
+    win.find('.sub-form').hide();
+
+    var form = win.find('#sub-form-' + id);
+    form.show();
+
+    win.toggleClass('is-open');
+    toggleOverlay(win);
+}
+
+function toggleShopWindow(id) {
+    var win = $('#w2');
+
+    if (id == 0) {
+        win.removeClass('is-open');
+        toggleOverlay(win);
+        return;
+    }
+
+    win.find('.shop-item').hide();
+
+    var item = win.find('#shop-item-' + id);
+    item.show();
+
+    var name = item.find('.push-window__title').text();
+    win.find('input[name=item]').val(name);
+
+    win.toggleClass('is-open');
+    toggleOverlay(win);
+}
+
+function toggleNav() {
+    var win = $('#w1');
+    win.toggleClass('is-open');
+    toggleOverlay(win);
+}
+
+function toggleOverlay(win) {
+    if (win.hasClass('is-open')) {
         createOverlay();
     } else {
         removeOverlay();
     }
-
 }
 
 function createOverlay() {
     var docHeight = $(document).height();
 
-    $("<div id='overlay'></div>")
+    $('body').addClass('overflow');
+
+    $("<div class='overlay'></div>")
         .appendTo("body")
         .height(docHeight)
         .css({
@@ -112,5 +141,6 @@ function createOverlay() {
 }
 
 function removeOverlay() {
-    $('#overlay').remove();
+    $('body').removeClass('overflow');
+    $('.overlay').remove();
 }
