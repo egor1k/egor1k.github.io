13f436fc192b032a87a576589894604bcbdf378a
egor1k <george.mainbox@gmail.com>
2019-02-01 16:02:52 +0300

    jspoli

--- polimob/js/main.js
+++ polimob/js/main.js
@@ -1,24 +1,14 @@
 "use strict";
 
-var slidePanel = {
-    selector: $('.slide-panel'),
-    activeClassName: 'is-open',
-    open: function () {
-        this.selector.addClass(this.activeClassName);
-    },
-    close: function () {
-        this.selector.removeClass(this.activeClassName);
-    }
-}
+var settings = {
+    toggle: ".button-hamburger", // the selector for the menu toggle, whatever clickable element you want to activate or deactivate the menu. A click listener will be added to this element.
+    exit_selector: ".button-breaker", // the selector for an exit button in the div if needed, when the exit element is clicked the menu will deactivate, suitable for an exit element inside the nav menu or the side bar
+    animation_duration: "0.4s", //how long it takes to slide the menu
+    place: "left", //where is the menu sliding from, possible options are (left | right | top | bottom)
+    animation_curve: "cubic-bezier(0.54, 0.01, 0.57, 1.03)", //animation curve for the sliding animation
+    body_slide: false, //set it to true if you want to use the effect where the entire page slides and not just the div
+    no_scroll: true, //set to true if you want the scrolling disabled while the menu is active
+    auto_close: true //set to true if you want the slider to auto close everytime a child link of it is clicked
+};
 
-$(function () {
-    $('.slide-panel').bind('mouseenter touchstart', function(e) {
-        var current = $(window).scrollTop();
-        $(window).scroll(function(event) {
-            $(window).scrollTop(current);
-        });
-    });
-    $('.slide-panel').bind('mouseleave touchend', function(e) {
-        $(window).off('scroll');
-    });
-});
+$('.slide-panel').sliiide(settings); //initialize sliiide
