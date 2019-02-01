b26601b33e530f537101375b88f2e3047f3cfbaa
egor1k <george.mainbox@gmail.com>
2019-02-01 16:14:43 +0300

    89

--- polimob/js/main.js
+++ polimob/js/main.js
@@ -6,7 +6,7 @@ var settings = {
     animation_duration: "0.4s", //how long it takes to slide the menu
     place: "left", //where is the menu sliding from, possible options are (left | right | top | bottom)
     animation_curve: "cubic-bezier(0.54, 0.01, 0.57, 1.03)", //animation curve for the sliding animation
-    body_slide: false, //set it to true if you want to use the effect where the entire page slides and not just the div
+    body_slide: true, //set it to true if you want to use the effect where the entire page slides and not just the div
     no_scroll: true, //set to true if you want the scrolling disabled while the menu is active
     auto_close: true //set to true if you want the slider to auto close everytime a child link of it is clicked
 };
