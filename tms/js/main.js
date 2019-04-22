3d7bcd8244f115cf6ee4a56bc12079332239163b
egor1k <george.mainbox@gmail.com>
2019-04-22 08:43:43 +0300

    123

--- tms/js/main.js
+++ tms/js/main.js
@@ -17,39 +17,6 @@ $(document).ready(function () {
         body.css('padding-top', headerHeight);
     }
 
-    $('.slider-partners').slick({
-        swipe: false,
-        slidesToShow: 5,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        cssEase: 'ease-in-out',
-        responsive: [{
-                breakpoint: 1229,
-                settings: {
-                    slidesToShow: 4
-                }
-            },
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 575,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
-
     $('.custom-file-input').on('change', function () {
         let fileName = $(this).val().split('\\').pop();
         $(this).next('.custom-file-label').addClass("selected").html(fileName);
