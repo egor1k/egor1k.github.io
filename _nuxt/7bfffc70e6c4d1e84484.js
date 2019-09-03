f7d68bef77276be94b08ca125d52e46fe24941fe
egor1k <george.mainbox@gmail.com>
2019-09-03 16:20:47 +0300

    mfc_pack

--- _nuxt/7bfffc70e6c4d1e84484.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{305:function(t,e,n){"use strict";n.r(e);var l=n(0),component=Object(l.a)({},function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"container"},[e("the-title",[this._v("Контакты")])],1)},[],!1,null,null,null);e.default=component.exports}}]);
--- _nuxt/9477703eb12c74c6c729.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{307:function(t,e,n){"use strict";n.r(e);var l=n(0),component=Object(l.a)({},function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"container"},[e("the-title",[this._v("Акции")])],1)},[],!1,null,null,null);e.default=component.exports}}]);
